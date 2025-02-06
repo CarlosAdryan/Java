b609001d3069818ba0d328baeb4f5f438eb92d56
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-02-06 14:06:36 -0300

    Casos de Teste

--- ws-eclipse/casos_de_teste/src/casos_de_teste/Main.java
+++ ws-eclipse/casos_de_teste/src/casos_de_teste/Main.java
@@ -11,18 +11,17 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
+		
 
 		for (int i = 0; i < N; i++) {
 
-			double v1 = sc.nextDouble();
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();	
+			double c = sc.nextDouble();
 
-			int peco1 = 2;
-			int peco2 = 3;
-			int peco3 = 5;
-			double media = (v1 * 2 + v1 * 3 + v1 * 5);
-			double pecototal = peco1 + peco2 + peco3;
-			double mediaponder1 = media / pecototal;
-			System.out.println(mediaponder1);
+			double media = (a * 2 + b * 3 + c * 5) / 10;
+			
+			System.out.printf("%.1f", media);
 		}
 
 		sc.close();
