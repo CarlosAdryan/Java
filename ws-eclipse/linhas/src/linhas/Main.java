9e4a88b55f580fe85088496d18185321a246ea1f
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-02-06 20:04:58 -0300

    Exercícios em progesso

--- ws-eclipse/linhas/src/linhas/Main.java
+++ ws-eclipse/linhas/src/linhas/Main.java
@@ -9,9 +9,12 @@ public class Main {
 		
 		int N = sc.nextInt();
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
+			int r = i;
+			r = i * i;
+			System.out.println(+ i + r);
 			
-			System.out.println(i);
+			
 			
 		}
 		
