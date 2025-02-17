ef6ed805e6e4482ed45922f5b32ba63f822992e2
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-02-17 11:43:16 -0300

    Exercício, o mais velho

---
+++ ws-eclipse/mais_velho/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantas pessoas voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		String [] vectname = new String[n];
+		int [] vectidd = new int[n];
+		
+		int maior = 0;
+		
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("Dados da %da pessoa:%n", i+1);
+			System.out.print("Nome: ");
+			sc.nextLine();
+			vectname[i] = sc.nextLine();
+			System.out.print("Idade: ");
+			vectidd[i] = sc.nextInt();
+		}
+		
+		maior = vectidd[0];
+		
+		for(int i=1; i<n; i++) {
+			if(vectidd[i] > maior) {
+				maior = vectidd[i];
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(vectidd[i] == maior) {
+				System.out.printf("PESSOA MAIS VELHA: %s", vectname[i]);
+			}
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
