687a04c3e2a09d13688eeb9979e46ebd3157044a
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-02-20 10:40:16 -0300

    Aulão 003 concluido

--- ws-eclipse/aulao003/src/application/Program.java
+++ ws-eclipse/aulao003/src/application/Program.java
@@ -25,6 +25,12 @@ public class Program {
 			System.out.println("Employee #" + (i + 1) + ":");
 			System.out.print("Id: ");
 			Integer id = sc.nextInt();
+			while(hasId(list, id)) {
+				System.out.println("Id alredy taken! Try again: ");
+				id = sc.nextInt();
+			}
+			
+			
 			System.out.print("Name: ");
 			sc.nextLine();
 			String name = sc.nextLine();
@@ -38,21 +44,27 @@ public class Program {
 		}
 		
 		System.out.println();
-		
 		System.out.print("Enter the employee id that will have salary increase : ");
 		int idsalary = sc.nextInt();
-		Integer pos = position(list, idsalary);
-		if(pos == null) {
+		
+		Employee emp = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
+		
+		//Integer pos = position(list, idsalary);
+		
+		if(emp == null) {
 			System.out.println("This id does not exist?");
 		}
 		else {
-			System.out.println("Enter the percentage: ");
+			System.out.print("Enter the percentage: ");
 			double percent = sc.nextDouble();
+			emp.increaseSalary(percent);
 		}
 		
-		
-		
-		
+		System.out.println();
+		System.out.println("List of employees: ");
+		for (Employee e : list) {
+			System.out.println(e);
+		}
 		
 		sc.close();
 
@@ -65,6 +77,12 @@ public class Program {
 			}
 		}
 		return null;
+	}
+	
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
 	}
 
 }
