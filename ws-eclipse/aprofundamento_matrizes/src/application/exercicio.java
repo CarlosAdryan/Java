8addf71db5007c28b9c5ed0950cfbb968c6f168a
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-02-22 15:16:00 -0300

    Exercicío em andamento

---
+++ ws-eclipse/aprofundamento_matrizes/src/application/exercicio.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual a ordem da matriz? ");
+		int n = sc.nextInt();
+		double[][] mat = new double[n][n];
+		
+		System.out.println("Digite os valores da matriz:");
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				mat[i][j] = sc.nextDouble();
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			double soma = mat[i][i] + mat[i][i];
+			System.out.println("Soma da diagonal principal =" + soma);
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
