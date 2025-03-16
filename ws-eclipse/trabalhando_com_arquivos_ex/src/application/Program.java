feb114eaed03f9c69fd295081f679e5e3afc9187
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-03-16 13:59:06 -0300

    Trabalhando com arquivos

--- ws-eclipse/trabalhando_com_arquivos_ex/src/application/Program.java
+++ ws-eclipse/trabalhando_com_arquivos_ex/src/application/Program.java
@@ -1,34 +1,9 @@
 package application;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		File file = new File("C:\\Users\\Carlos\\Documents\\estudos\\Java\\ws-eclipse\\trabalhando_com_arquivos_ex\\txt.csv");
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
-		}
-		catch (IOException e) {
-			System.out.println("Erro: " + e.getMessage());
-		}
-		finally {
-			if (sc != null) {
-				sc.close();
-			}
-		}
-		
-		sc.close();
-
 	}
-
+	
 }
