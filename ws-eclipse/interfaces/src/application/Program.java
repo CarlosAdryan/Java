abb43e93d12af89bd4b6bdd261e4cddf24e24604
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-03-16 19:06:16 -0300

    .

--- ws-eclipse/interfaces/src/application/Program.java
+++ ws-eclipse/interfaces/src/application/Program.java
@@ -1,16 +1,24 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter rental data");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
+		System.out.println("Enter rental data");
+		System.out.print("Model car: ");
+		String carModel = sc.nextLine();
+		System.out.println("Withdrawal (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start;
 		
 		
 		sc.close();
