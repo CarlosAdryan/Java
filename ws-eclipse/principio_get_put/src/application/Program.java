9304c1b80f1badda46038c96a6f6dc31905b7834
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-03-22 14:56:08 -0300

    Contravariância

--- ws-eclipse/principio_get_put/src/application/Program.java
+++ ws-eclipse/principio_get_put/src/application/Program.java
@@ -1,9 +1,22 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		
+		List<? super Number> myNums = myObjs;
+		
+		myNums.add(10);
+		myNums.add(3.14);
+		
+		Number x = myNums.get(0); // erro de compilacao
 
 	}
 
