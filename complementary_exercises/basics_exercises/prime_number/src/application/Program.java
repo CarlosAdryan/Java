d248adb379e93cdf9b7caccb034331b71c64eaa5
Carlos Adryan <carlosadryana@gmail.com>
2025-04-17 11:30:22 -0300

    Prime numbers

--- complementary_exercises/basics_exercises/prime_number/src/application/Program.java
+++ complementary_exercises/basics_exercises/prime_number/src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
 		
+		int prime = 0; 
+		if (n >= 1) {
+			for (int i=2; i <= Math.sqrt(n); i++) {
+				if (n % i == 0) {
+					prime = 1;
+				} 
+			}
+			if (prime == 0) {
+				System.out.println("Not prime");
+			} else {
+				System.out.println("Prime");
+			}
+		}
+		sc.close();	
 	}
-
 }
