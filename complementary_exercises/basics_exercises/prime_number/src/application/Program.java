c342eb08058b87932286fa24161cbb6b0a6cbfa4
Carlos Adryan <carlosadryana@gmail.com>
2025-04-18 11:46:46 -0300

    Check if the number is prime

--- complementary_exercises/basics_exercises/prime_number/src/application/Program.java
+++ complementary_exercises/basics_exercises/prime_number/src/application/Program.java
@@ -11,17 +11,20 @@ public class Program {
 		int n = sc.nextInt();
 		
 		int prime = 0; 
-		if (n >= 1) {
+		if (n >= 2) {
 			for (int i=2; i <= Math.sqrt(n); i++) {
 				if (n % i == 0) {
-					prime = 1;
+					prime = 1; // achou um divisor => NÃO é primo
+					break; // achou um divisor, não precisa continuar
 				} 
 			}
-			if (prime == 0) {
-				System.out.println("Not prime");
-			} else {
+			if (prime != 1) { // ou seja, se NÃO achou nenhum divisor
 				System.out.println("Prime");
+			}else {
+				System.out.println("Not prime");
 			}
+		} else {
+			System.out.println("Not prime");
 		}
 		sc.close();	
 	}
