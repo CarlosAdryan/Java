3ddd4de2ae846b1a7015bdaad4d86c3acc0dacea
Carlos Adryan <carlosadryana@gmail.com>
2025-04-22 13:01:14 -0300

    Progress

--- complementary_exercises/basics_exercises/Numeric_Palindrome/src/application/Program.java
+++ complementary_exercises/basics_exercises/Numeric_Palindrome/src/application/Program.java
@@ -11,7 +11,11 @@ public class Program {
 		System.out.println("Enter a number");
 		int n = sc.nextInt();
 		
+		String palavra = Integer.toString(n);
+		
+		for (int i=0; i != palavra.charAt(i); i++) {
 			
+		}
 		
 		sc.close();
 	}
