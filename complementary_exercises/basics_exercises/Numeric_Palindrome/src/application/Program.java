9df704083c31953d0ca570f595371575ef59fe98
Carlos Adryan <carlosadryana@gmail.com>
2025-04-23 11:45:51 -0300

    Changing the data type to 'long' to support large numbers in the palindrome algorithm

--- complementary_exercises/basics_exercises/Numeric_Palindrome/src/application/Program.java
+++ complementary_exercises/basics_exercises/Numeric_Palindrome/src/application/Program.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-	
-		System.out.print("Enter a number: ");
-		int n = sc.nextInt();
-		
-		String palavra = Integer.toString(n);
-		
-		boolean ehPalindromo = true;
-		
-		for (int i=0; i < palavra.length() / 2; i++) {
-			if (palavra.charAt(i) != palavra.charAt(palavra.length() - 1 - i)) {
-				ehPalindromo = false;
-				break; // não precisa continuar se já sabe que não é
-			}
-		}
-		
-		if (ehPalindromo) {
-			System.out.println("Is palindrome!");
-		} else {
-			System.out.println("not palindrome.");
-		}
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        long n = sc.nextLong();  // Mudado para long para aceitar números grandes
+
+        String palavra = Long.toString(n);  // Mudança para usar Long.toString()
+
+        boolean ehPalindromo = true;
+
+        for (int i = 0; i < palavra.length() / 2; i++) {
+            if (palavra.charAt(i) != palavra.charAt(palavra.length() - 1 - i)) {
+                ehPalindromo = false;
+                break; // não precisa continuar se já sabe que não é
+            }
+        }
+
+        if (ehPalindromo) {
+            System.out.println("Is palindrome!");
+        } else {
+            System.out.println("Not palindrome.");
+        }
+
+        sc.close();
+    }
 }
