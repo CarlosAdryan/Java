e2682ec57560d584a6fbe06c00a6e6f98446ad1f
Carlos Adryan <carlosadryana@gmail.com>
2025-04-24 11:28:19 -0300

    Improved clarity of input and output

--- complementary_exercises/exercises/Valid_Triangle/src/application/Program.java
+++ complementary_exercises/exercises/Valid_Triangle/src/application/Program.java
@@ -7,22 +7,23 @@ public class Program {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter segments");
-        System.out.print("AB: ");
+
+        // Solicita os segmentos do triângulo
+        System.out.println("Enter the segments of the triangle:");
+        System.out.print("Side AB: ");
         int ab = sc.nextInt();
-        System.out.print("CD: ");
+        System.out.print("Side CD: ");
         int cd = sc.nextInt();
-        System.out.print("EF: ");
+        System.out.print("Side EF: ");
         int ef = sc.nextInt();
         
-        // Verificando a validade do triângulo
+        // Verificação se os lados formam um triângulo válido
         if (ab + cd > ef && ab + ef > cd && cd + ef > ab) {
-            System.out.println("Forms a triangle");
+            System.out.println("The sides form a triangle.");
         } else {
-            System.out.println("Doesn't form a triangle");
+            System.out.println("The sides do not form a triangle.");
         }
-
+        
         sc.close();
     }
 }
